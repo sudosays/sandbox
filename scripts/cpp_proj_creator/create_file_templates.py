691c80880eec6155dbb07a639adefb46b5e5ee00
Julius Stopforth <jjstopforth@gmail.com>
2016-02-24 08:18:54 +0200

    Add basic templating funcionality to py script and basic gitignore files.

--- scripts/cpp_proj_creator/create_file_templates.py
+++ scripts/cpp_proj_creator/create_file_templates.py
@@ -2,12 +2,34 @@
 # Julius Stopforth
 # 18.02.2016
 
-import sys;
+import sys
+from string import Template
+from datetime import date
 
-project_name = sys.arv[1]
+# Formats given date as DD.MM.YYYY
+def format_date(a_date):
+	
+	datestr = str(a_date.day).zfill(2) + "."
+	datestr += str(a_date.month).zfill(2) + "."
+	datestr += str(a_date.year).zfill(4)
+	return datestr
 
 
+project_name = sys.argv[1]
+
+default_author = "Julius Stopforth"
+
+creation_date = format_date(date.today())
+
+template_dict = {'project_name': project_name, "author": default_author, "date": creation_date}
+
+print(template_dict)
 
 
+temp_file = open( 'file.template' )
 
+src = Template( temp_file.read())
 
+result = src.substitute(template_dict)
+
+print( result )
