7ad064568b9c5c5941aee07228a28df53c696087
Julius Stopforth <jjstopforth@gmail.com>
2016-02-25 08:18:20 +0200

    Move system functions into python script so that it is self-contained.

--- scripts/cpp_proj_creator/create_file_templates.py
+++ scripts/cpp_proj_creator/create_file_templates.py
@@ -2,26 +2,47 @@
 # Julius Stopforth
 # 18.02.2016
 
-import sys
+
+# add os.exec for git repo initialisation
+# use os.exec for dir creation etc
+
+import subprocess
 from string import Template
 from datetime import date
 
 # Formats given date as DD.MM.YYYY
 def format_date(a_date):
 	
-	datestr = str(a_date.day).zfill(2) + "."
-	datestr += str(a_date.month).zfill(2) + "."
+	datestr = str(a_date.day).zfill(2) + '.'
+	datestr += str(a_date.month).zfill(2) + '.'
 	datestr += str(a_date.year).zfill(4)
 	return datestr
 
+## Ask the user for the name of the project ##
+project_name = input('Please enter the name of the project:\n')
 
-project_name = sys.argv[1]
 
-default_author = "Julius Stopforth"
+## Create the project directory ##
+print('Attempting to create directory called ' + project_name + '.')
+
+try:
+	subprocess.run(['mkdir', projectname])
+	print('Success')
+except SubprocessError:
+	print('Failed')
+	raise
+	
+	
+	
+
+
+default_author = 'Julius Stopforth'
 
 creation_date = format_date(date.today())
 
-template_dict = {'project_name': project_name, "author": default_author, "date": creation_date}
+base_dict = {'project_name': project_name, 'author': default_author, 'date': creation_date}
+
+makefile_dict = {}
 
 print(template_dict)
 
