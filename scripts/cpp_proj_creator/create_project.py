8f5242d571035e71da95152f256226b602e85b87
Julius Stopforth <jjstopforth@gmail.com>
2016-03-06 11:38:43 +0200

    Add file generation from templates given filenames
    The program now correctly generates the file names from an array of
    specified filenames. The makefile is generated by default. However,
    the files are being dumped into the parent directory instead of the
    project directory. This must needs be fixed.

--- scripts/cpp_proj_creator/create_project.py
+++ scripts/cpp_proj_creator/create_project.py
@@ -20,6 +20,9 @@ from os import chdir
 user_data = {}
 MAIN_METHOD = 'int main($param)\n{\n\t\n\treturn 0;\n}\n'
 
+template_files = {'source':'source.template','header':'header.template','makefile':'makefile.template'}
+
+
 def config():
 	try:
 		config_file = open( 'config.json' )
@@ -39,18 +42,65 @@ def format_date(a_date):
 	datestr += str(a_date.year).zfill(4)
 	return datestr
 
+def gen_identifier(astr):
+	newstr = astr.strip() # Remove whitespace
+	newstr = newstr.upper()
+	if ('.' in newstr):
+		newstr = newstr.replace('.','_')
+	else:
+		newstr += '_H'
+	return newstr
+		
+
 def create_dir(dirname):
 	try:
 		subprocess.call(['mkdir', dirname])
 	except SubprocessError:
 		print('Error: directory creation failed!')
 		raise
-		
+
+def create_file_from_template(filetype, filename, extension=''):
+
+	temp_file = open( template_files[filetype] )
+	raw = Template( temp_file.read())
+	temp_file.close()
+	
+	if (filetype == 'header'):
+		user_data['header_identifier'] = gen_identifier(filename)
+
+	data = raw.safe_substitute(user_data)
+	
+	if (filename == 'driver' and filetype == 'source'):
+		data += MAIN_METHOD
+	
+	output = open(filename + extension, 'w')
+	output.write(data)
+	output.close()
+	
+	
+def gen_files(files):
+	# Keep track of files generated for makefile
+	# Will later be used to generate $objects and $sources strings
+	for filename in files:
+		create_file_from_template('source', filename, '.cpp')
+		create_file_from_template('header', filename, '.h')	
+				
+	files.append('')
+	user_data['objects'] = '.o '.join(files).strip()
+	user_data['sources'] = '.cpp '.join(files).strip() 	 
+	
+	create_file_from_template('makefile', 'makefile')	
+	
 def git_setup():
+	# Initialise the repository
 	subprocess.call(['git', 'init'])
-	## Once the git repo has been created set the local user settings
+	# Once the git repo has been created set the local user settings
 	subprocess.call(['git', 'config', '--local', 'user.name', '"' + user_data['author'] + '"'])
 	subprocess.call(['git', 'config', '--local', 'user.email', user_data['email']])	
+	# Automate the initial commit
+	# ENSURE FILES HAVE BEEN CREATED?	
+	#subprocess.call(['git', 'commit', '-m', 'Initial commit']) 
+	
 
 if __name__ == "__main__":
 
@@ -62,8 +112,23 @@ if __name__ == "__main__":
 	project_name = input('Please enter the name of the project:\n')
 
 
-	## Create the project directory (parent) ##
+	# Create the project directory (parent) ##
 	print('Attempting to create directory called ' + project_name + '.')
 	create_dir(project_name)
-	chdir(project_name)
-	git_setup()
+	
+	# Change into the newly created directory 
+	# chdir(project_name)
+	
+	# Files to generate:
+	# - driver.cpp && driver.h
+	# - sourcefile.cpp && source.h
+	# - makefile incl. above files 
+	
+	files = [project_name, 'driver']	
+	gen_files(files)
+	
+	
+	# Initialise the git repository
+	# git_setup()
+	
+	
