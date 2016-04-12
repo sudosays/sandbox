212f82296e47d334ff46d8f270101387f6ca26d5
Julius Stopforth <jjstopforth@gmail.com>
2016-04-12 11:13:25 +0200

    Create breaktherules directory

---
+++ misc/breaktherules/2016/pangram.py
@@ -0,0 +1,11 @@
+# Pangram verifier
+# Created to apply for #BreaktheRules 2016
+# Author: Julius Stopforth
+# Date: 12.04.2016
+
+def pangram_check(aString):
+	aString = aString.upper()
+	
+	
+
+	return false
