e65bf891215c75a01834a8b3314edd3aa13855b5
Julius Stopforth <jjstopforth@gmail.com>
2016-04-12 11:32:17 +0200

    Complete first working ver of pangram solution

--- misc/breaktherules/2016/pangram.py
+++ misc/breaktherules/2016/pangram.py
@@ -3,9 +3,17 @@
 # Author: Julius Stopforth
 # Date: 12.04.2016
 
-def pangram_check(aString):
-	aString = aString.upper()
-	
-	
 
-	return false
+def pangram_check(aString):
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	
+	aString = aString.lower()
+	
+	tempstr = ""
+
+	for letter in aString:
+		if ((letter not in tempstr) and (letter in alphabet) ):
+			tempstr += letter
+	tempstr = ''.join(sorted(tempstr))
+	print(tempstr)
+	return (alphabet == tempstr)
