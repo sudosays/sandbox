51375eba3ca83b9b0b01ec961712880d52e768b9
Julius Stopforth <jjstopforth@gmail.com>
2016-10-25 13:52:37 +0200

    Add opengl tutorial project
    Started the tutorial from learnopengl.com and added it to the sandbox
    workspace. This should hopefully be used as a reference for future OGL
    projects. The GLFW and GLEW libs are in the 'libs' folder for copying.

---
+++ graphics/3D/openGL/Tutorial/tutorial.cpp
@@ -0,0 +1,25 @@
+/**
+
+Main file
+
+Author: Julius Stopforth
+Date: 25.10.2016
+
+The following project was created by following the learnopengl.com tutorial
+
+**/
+
+
+// This is necessary otherwise GLEW will be dynamically linked instead
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+
+int main(void)
+{
+
+    return 0;
+
+}
